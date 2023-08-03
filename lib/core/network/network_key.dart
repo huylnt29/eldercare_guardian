ff8f446e8eb2e91cbb0318a51480d8c4d64fb61a
huylnt20127037 <20127037@student.hcmus.edu.vn>
2023-08-03 16:38:17 +0700

    removed redundant file

--- lib/core/network/network_key.dart
+++
@@ -1,9 +0,0 @@
-class NetworkKey {
-  NetworkKey._internall();
-  static const String listMaximumSentOtp = 'listMaximumSentOtp';
-  static const String accessToken = 'access_token';
-  static const String refreshToken = 'refresh_token';
-  static const String userProfileId = 'user_profile';
-  static const String account = 'account';
-  static const String loginWithBiometric = 'user_login_biometric';
-}
